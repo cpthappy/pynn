ec5b6ba506ff595d877ab8c66d8f8901d04af801
Georg Vogelhuber <vogelhuber@gmx.de>
2017-12-31 18:25:59 +0100

    methods in mnist

--- mnist_nn.py
+++ mnist_nn.py
@@ -1,28 +1,37 @@
 import csv
-import os
-import pprint
-import network
 import numpy
+import os
+import network
+
 
 def read_dataset(path):
     dataset = []
-    with open(path, "r") as f:
-        reader = csv.reader(f)
+    with open(path, "r") as in_file:
+        reader = csv.reader(in_file)
         for line in reader:
             converted = map(int, line)
             dataset.append((converted[0], converted[1:]))
     return dataset
 
 def scale_dataset(data_list, max_value):
-    result = []
+    result_list = []
     for label, data in data_list:
-        result.append((label, [1.0 * x/max_value + 0.01 for x in data]))
-    return result
+        result_list.append((label, [1.0 * x/max_value + 0.01 for x in data]))
+    return result_list
 
 def create_target(label, output_nodes):
-    target = numpy.zeros(output_nodes) + 0.1
-    target[label] = 0.99
-    return target
+    target_vector = numpy.zeros(output_nodes) + 0.1
+    target_vector[label] = 0.99
+    return target_vector
+
+def train_model(model, train_data):
+    for label, entry in train_data:
+        target = create_target(label, 10)
+        model.train(entry, target)
+
+def test_model(uut, test_data):
+    count = sum([uut.query(entry).argmax() == label for label, entry in test_data])
+    return 1.0*count/len(test_data)
 
 if __name__ == '__main__':
     TRAIN_DATA = read_dataset(os.path.join("data", "mnist_train_100.csv"))
@@ -30,11 +39,6 @@ if __name__ == '__main__':
     TEST_DATA = read_dataset(os.path.join("data", "mnist_test_10.csv"))
     TEST_DATA = scale_dataset(TEST_DATA, 255.0)
     NETWORK = network.NeuralNetwork(784, 200, 10, 0.3)
-    
-    for label, entry in TRAIN_DATA:
-        target = create_target(label, 10)
-        NETWORK.train(entry, target)
-
-    for label, entry in TEST_DATA:
-        result = NETWORK.query(entry)
-        print label, result.argmax()
+    train_model(NETWORK, TRAIN_DATA)
+    print test_model(NETWORK, TEST_DATA)
+   
